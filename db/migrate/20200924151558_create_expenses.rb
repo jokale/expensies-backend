b28f7439b43c56b3a0558145442d256cd035df6b
jokale <kalemajoanna@gmail.com>
2020-09-24 16:16:41 +0100

    create expense resource

---
+++ db/migrate/20200924151558_create_expenses.rb
@@ -0,0 +1,12 @@
+class CreateExpenses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :expenses do |t|
+      t.string :title
+      t.datetime :date
+      t.text :details
+      t.integer :amount
+
+      t.timestamps
+    end
+  end
+end
