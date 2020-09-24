5eadfbc788f571a13ed5a3c8639c7ddd2b1c6780
jokale <kalemajoanna@gmail.com>
2020-09-24 16:38:40 +0100

    create user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render json:@users
+    end 
+
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            render json:@user
+        else 
+            render json: {error: 'Error creating that User entry please try again'}
+
+        end
+    end 
+
+    def show
+        @user = User.find(params[:id])
+        render json:@user
+    end 
+
+    def update
+    end 
+
+    def destroy
+    end 
+
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username,:email, :password_digest)
+    end 
+
+
 end
