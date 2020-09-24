6e2004ab1955ff4ff84cfe2a3d12af5c5d284ccd
jokale <kalemajoanna@gmail.com>
2020-09-24 16:26:30 +0100

    create expense controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,37 @@
 class ExpensesController < ApplicationController
+
+    def index
+        @expenses = Expense.all
+        render json:@expenses
+    end 
+
+
+    def create 
+        @expense = Expense.new(expense_params)
+        if @expense.save
+            render json:@expense
+        else 
+            render json: {error: 'Error creating that expense entry please try again'}
+
+        end
+    end 
+
+    def show
+        @expense = Expense.find(params[:id])
+        render json:@expense
+    end 
+
+    def update
+    end 
+
+    def destroy
+    end 
+
+
+    private 
+
+    def expense_params
+        params.require(:expense).permit(:title,:amount, :date, :details)
+    end 
+
 end
